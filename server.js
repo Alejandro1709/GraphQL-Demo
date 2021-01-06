362f1fda65533953a20eac96592f26d9197b04ac
Alejandro <alejandro.lr.developer@hotmail.com>
2021-01-06 14:25:32 -0500

    DONE: App

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+const cors = require('cors');
 const schema = require('./schema');
-
+const path = require('path');
 const app = express();
+
+app.use(cors());
 
 app.use(
   '/graphql',
@@ -12,6 +15,11 @@ app.use(
   })
 );
 
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 const port = process.env.PORT || 5000;
 
 app.listen(port, () =>
